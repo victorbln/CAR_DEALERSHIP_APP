9d1d70f7c56bb0337a1437f77dbb8878c8307334
Victor Balan <victor.balan@student.usv.ro>
2023-03-16 12:37:02 +0200

    added models

---
+++ CAR_DEALERSHIP_APP/Models/Address.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+    class Address
+    {
+    private string? City { get; set; }= string.Empty;
+    private string? Region { get; set; }= string.Empty;
+    private string? PostalCode { get; set; }= string.Empty;
+    private string? Country { get; set; }= string.Empty;
+    public Address() { }
+    }
