7ebd85510512791cccff54567e42a81b415b96a6
Victor Balan <victor.balan@student.usv.ro>
2023-03-16 12:31:49 +0200

    Init

---
+++ CAR_DEALERSHIP_APP/Models/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class User
+{
+    public int Id { get; set; }
+    private string? Role { get; set; }
+    private string? Age { get; set; }
+    private string? Name { get; set; }
+    private string? Password { get; set; }
+    private string? Email { get; set; }
+    private string? PhoneNumber { get; set; } = string.Empty;
+    private Address Address { get; set; }
+
+    public User()
+	{
+		this.Address = new Address();
+	}
+}
---
+++ CAR_DEALERSHIP_APP/Models/Vehicle.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class Vehicle
+{
+	public int? Id { get; set; }
+	public string? Name { get; set; }
+	public string? Description { get; set; }
+	public string? Year { get; set; }
+	public string? Producer { get; set; }
+	public int? Amount { get; set; } = 0;
+
+	public Vehicle()
+	{
+	}
+
+}
