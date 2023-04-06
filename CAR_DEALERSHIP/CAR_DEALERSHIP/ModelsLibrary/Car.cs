a25d0d7cca7e7aed432e102de0f75bdc2c0b1b9f
victorbln <106177662+victorbln@users.noreply.github.com>
2023-04-06 10:04:56 +0300

    commit

---
+++ CAR_DEALERSHIP/CAR_DEALERSHIP/ModelsLibrary/Car.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CAR_DEALERSHIP.ModelsLibrary
+{
+    public class Car
+    {
+
+        public Car() {
+            this.Name = this.Year = this.Producer = this.Description = string.Empty;
+        }
+        public int? Id { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public string? Year { get; set; }
+        public string? Producer { get; set; }
+        public int? Amount { get; set; } = 0;
+    }
+}
--- CAR_DEALERSHIP/CAR_DEALERSHIP/Program.cs
+++ CAR_DEALERSHIP/CAR_DEALERSHIP/Program.cs
@@ -1,2 +1,101 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using CAR_DEALERSHIP.DataStorageLevel;
+
+using CAR_DEALERSHIP.ModelsLibrary;
+
+namespace CAR_DEALERSHIP
+{
+    class Program
+    {
+        static void Main()
+
+        {
+            Car car = new();
+            //string storageFile = ConfigurationManager.AppSettings["NumeFisier"];
+            CarsInventoryManager inventoryManager = new CarsInventoryManager("asd");
+            int nrCars = 0;
+            int idCar = 0;
+
+            inventoryManager.GetCars(out nrCars);
+            string option;
+            do
+            {
+            
+                Console.WriteLine("A. Add car to inventory");
+                Console.WriteLine("I. Show inventory");
+                Console.WriteLine("P. Show car properties");
+                Console.WriteLine("F. Show last month sales");
+                Console.WriteLine("S. Save inventory");
+                Console.WriteLine("D. Sell car");
+                Console.WriteLine("X. Stop program");
+                Console.WriteLine("Choose an option: ");
+           
+                option = Console.ReadLine();
+                switch (option.ToUpper())
+                {
+                    case "A":
+                        car = GetCarFromKeyboard();
+
+                        break;
+                    case "I":
+                        ShowInventory();
+
+                        break;
+                    case "P":
+                        ShowCarProperties(idCar);
+
+                        break;
+                    case "F":
+
+                        ShowLastMonthSales();
+
+                        break;
+                    case "S":
+                        
+                        car.Id = nrCars + 1;
+                        
+                        inventoryManager.AddCars(car);
+
+                        nrCars = nrCars + 1;
+
+                        break;
+                    case "D":
+                        sellCar(idCar);
+
+                        break;
+                    case "X":
+                        return;
+                    default:
+                        Console.WriteLine("Invalid option!");
+                        break;
+                }
+            } while (option.ToUpper() != "X");
+
+            Console.ReadKey();
+        }
+
+        private static void sellCar(int idCar)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void ShowLastMonthSales()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void ShowCarProperties(int idCar)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void ShowInventory()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static Car GetCarFromKeyboard()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
